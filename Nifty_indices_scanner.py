b2446e18961078bc48728b54e67ad0b1394faad1
krishnateja08 <krishnateja08@gmail.com>
2026-02-26 19:21:29 -0600

    Update Nifty_indices_scanner.py

--- Nifty_indices_scanner.py
+++ Nifty_indices_scanner.py
@@ -167,7 +167,7 @@ sectors_config = {
         }
     },
     "Nifty Consumer Durables": {
-        "ticker": "^CNXCONSDURABLES",
+        "ticker": "^NIFTY_CONSR_DURBL.NS",
         "stocks": {
             "TITAN.NS":      {"weight": 25.0, "industry": "Jewelry & Watches"},
             "VOLTAS.NS":     {"weight": 12.0, "industry": "Air Conditioners"},
